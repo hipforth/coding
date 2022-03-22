9c9e83026ff8d2efd582815fedc58f55dd26f86f
hipforth <hpfnuaa@outlook.com>
2022-03-23 00:15:11 +0800

    update

--- desktop/二叉树/二叉树的层序遍历.cpp
+++ desktop/二叉树/二叉树的层序遍历.cpp
@@ -39,7 +39,29 @@ private:
     }
 unordered_map<int, vector<int> > mp;  
 };
-
+// 2. 广度优先搜索
+class Solution1 {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector <vector <int>> ret;
+        if (!root) {
+            return ret;
+        }
+        queue <TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int currentLevelSize = q.size();
+            ret.push_back(vector <int> ());
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                auto node = q.front(); q.pop();
+                ret.back().push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        return ret;
+    }
+};
 
 int main()
 {
